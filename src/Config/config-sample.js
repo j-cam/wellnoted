dff764af508f905a0b37a6bb9274ee188667b0bc
Jamie Campbell <jamie@spiritedcreative.com>
2018-02-08 16:06:46 -0800

    Added firebase dependency

---
+++ src/Config/config-sample.js
@@ -0,0 +1,9 @@
+// FireBase
+export const DB_CONFIG = {
+    apiKey: "xxxxxxx",
+    authDomain: "xxxxxxx",
+    databaseURL: "xxxxxxx",
+    projectId: "xxxxxxx",
+    storageBucket: "xxxxxxx",
+    messagingSenderId: "xxxxxxx"
+}
