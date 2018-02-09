f323f97fbb26e6e7717cd6853a7b14d1b498bfc8
Jamie Campbell <jamie@spiritedcreative.com>
2018-02-08 16:09:01 -0800

    Updated add/remove methods to sync with base

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { DB_CONFIG } from './Config/config';
+import firebase from 'firebase/app';
+import 'firebase/database';
 import './App.css';
 import Note from './Components/Note/note';
 import NoteForm from './Components/NoteForm/noteForm';
@@ -7,36 +10,75 @@ class App extends Component {
   
   constructor(props) {
     super(props);
+    this.app = firebase.initializeApp(DB_CONFIG);
+    this.database = this.app.database().ref('notes');
     this.addNote = this.addNote.bind(this);
+    this.removeNote = this.removeNote.bind(this);
 
     this.state = {
-      notes: [
-        {id: 1, title: "Note One Title", content: "Note body content..."},
-        {id: 2, title: "Title to Note 2", content: "Note body content..."}
-      ],
+      notes: [],
     }
   }
 
-  addNote(note) {
-    console.log('Adding a new note' + note);
-    const notes = this.state.notes;
-    note.id = `note-${Date.now()}`;
-    note.slug = `note-${notes.length + 1}`;
-    console.log(JSON.stringify(note));
-    notes.push(note);
-    this.setState({
-      notes: notes
+  componentWillMount() {
+    const theNotes = this.state.notes;
+
+    // DataSnapshot: on new note add
+    this.database.on('child_added', snapshot => {
+
+      theNotes.push({
+        id: snapshot.key,
+        title: snapshot.val().title,
+        content: snapshot.val().content
+      })
+
+      this.setState({
+        notes: theNotes
+      })
+
     })
-  
+
+    // DataSnapshot: on note delete
+    this.database.on('child_removed', snapshot => {
+
+      for (var i = 0; i < theNotes.length; i++) {
+        if (theNotes[i].id === snapshot.key) {
+          theNotes.splice(i, 1);
+        }
+      }
+
+      this.setState({
+        notes: theNotes
+      })
+    })
+
   }
-  
+  // componentDidMount() {}
+
+  addNote(note) {
+    this.database.push().set({
+      title: note.title,
+      content: note.content
+    })
+  }
+
+  removeNote(noteId) {
+    this.database.child(noteId).remove();
+  }
+
   render() {
     return (
       <div>
       {
         this.state.notes.map((note) => {
           return (
-            <Note key={note.id} id={note.id} title={note.title} content={note.content} />
+            <Note 
+              key={note.id} 
+              id={note.id} 
+              title={note.title} 
+              content={note.content}
+              removeNote={this.removeNote} 
+            />
           );
         })
       }
