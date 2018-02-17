0e1120cf95de822ee8c24438f928f3cda04e2660
Jamie Campbell <jamie@spiritedcreative.com>
2018-02-16 17:38:12 -0800

    Cleaned up note single

--- src/Components/NoteSingle/note-single.jsx
+++ src/Components/NoteSingle/note-single.jsx
@@ -4,23 +4,28 @@ import { Link } from "react-router-dom";
 
 
 class NoteSingle extends React.Component {
-    constructor(){
-        super();
-
-    }
 
     render(key) {
-        const index = this.props.match.params.noteId;
-        const details = this.props.notes[index];
-        // {console.log(this.props)}
+
+      const note = this.props.note;
+      const id = this.props.index; // needed later for editable
+      const creationDate = typeof note.timestamp !== 'undefined' && note.timestamp.length !== 0;
+
 
         return (
             <div className="note-single">
                 <Link to="/notes" className="note-single__back">back to your notes</Link>
 
                 <div className="note-single__body">
-                    <h1>{details.title}</h1>
-                    <p>{details.content}</p>
+                    <h1>{note.title}</h1>
+                    <span className="note__date">
+                      {
+                        creationDate
+                        ? [this.props.formatDate(note.timestamp), " at ", this.props.formatTime(note.timestamp)]
+                        : ''
+                      }
+                    </span>
+                    <p>{note.content}</p>
                 </div>
 
             </div>
